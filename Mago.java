8e2ac3ea84e656040949683911fe8c470a0855c7
Dannyson <DannysonK@hotmail.com>
2026-07-28 19:25:03 -0300

    Mago.java

--- Mago.java
+++ Mago.java
@@ -1,14 +1,36 @@
-// Subclasse que representa um personagem do tipo Mago
-public class Mago extends Personagem {
+import java.util.ArrayList;
+
+// Subclasse que representa um personagem do tipo Mago.
+// É a única subclasse que implementa a interface Auditavel, registrando suas ações.
+public class Mago extends Personagem implements Auditavel {
+
+    // Lista que guarda o histórico de ações realizadas pelo Mago
+    private ArrayList<String> acoes;
 
     // Construtor que usa super() para inicializar os dados herdados da superclasse
     public Mago(String nome, int nivel, int pontosDeVida, double poderBase) {
         super(nome, "Mago", nivel, pontosDeVida, poderBase);
+        this.acoes = new ArrayList<>();
     }
 
     // Implementação específica da habilidade do Mago
     @Override
     public void usarHabilidade() {
-        System.out.println("Habilidade: " + getNome() + " conjura um feitiço de proteção!");
+        System.out.println("Habilidade: " + getNome() + " conjura uma bola de fogo!");
+    }
+
+    // Adiciona uma nova ação ao histórico do Mago
+    @Override
+    public void registrarAcao(String acao) {
+        acoes.add(acao);
+    }
+
+    // Exibe todas as ações registradas pelo Mago
+    @Override
+    public void auditarAcoes() {
+        System.out.println("Ações auditadas:");
+        for (String acao : acoes) {
+            System.out.println("- " + acao);
+        }
     }
 }
